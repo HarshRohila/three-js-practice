6eff69d1142faa6964b4990e36efbf15d1d577de
Harsh Rohila <rohilaharsh@gmail.com>
2022-01-16 18:02:26 +0530

    added three js and scss

---
+++ src/components/app-home/app-home.tsx
@@ -0,0 +1,19 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'app-home',
+  styleUrl: 'app-home.scss',
+  shadow: true,
+})
+export class AppHome {
+  render() {
+    return (
+      <div class="app-home">
+        <stencil-route-link url="/profile/stencil">
+          <button>Profile page</button>
+        </stencil-route-link>
+        <div id="scene-container"></div>
+      </div>
+    );
+  }
+}
